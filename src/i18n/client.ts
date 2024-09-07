81e02ad912b58756b475667e523f382841d83864
Tears Mysthrala <unaiup@gmail.com>
2024-09-07 22:23:57 +0200

    lang solve

--- src/i18n/client.ts
+++ src/i18n/client.ts
@@ -2,7 +2,7 @@ import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
 // Importación estática del archivo de localización
-import en from '../locales/en.json';
+import en from '../../public/locales/en/common.json';
 
 i18next
   .use(initReactI18next)
@@ -10,7 +10,7 @@ i18next
     resources: {
       en: { translation: en },
     },
-    lng: 'en', // idioma por defecto
+    lng: 'es', // idioma por defecto
     fallbackLng: 'en',
     interpolation: {
       escapeValue: false,
