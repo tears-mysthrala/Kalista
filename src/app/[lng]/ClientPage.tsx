0d334acbfa3b29489088e3c8bb6b856d2736a8fb
Tears Mysthrala <unaiup@gmail.com>
2024-09-07 20:13:33 +0200

    a

---
+++ src/app/[lng]/ClientPage.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+function ClientPage() {
+  // ... otro código ...
+
+  const condicion = true; // Definimos la variable condición
+
+  if (condicion) {
+    return null;
+  }
+    
+  return (
+    <div>
+      {/* ... JSX aquí ... */}
+    </div>
+  );
+}
+
+export default ClientPage;
